dcb55afce63c9b31c9a7622465d948b373e0bb6f
Tomi Javanainen <tomi.javanainen@proton.me>
2025-05-09 15:30:51 +0300

    Add script to compile and install to /usr/bin

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+PERMS=755
+DEST=/usr/bin
+if [[ -z "${VIRTUAL_ENV}" ]]; then
+  echo "Please activate venv"
+  exit 1
+fi
+
+read -p "Install discord-notify (and link notify-discord) to ${DEST}? (y/n) " yn
+case $yn in
+  [Yy]* )
+    pyinstaller --onefile notify.py
+    cp -v dist/notify "${DEST}/discord-notify"
+    chmod -v $PERMS "${DEST}/discord-notify"
+    ln -s -f -v "${DEST}/discord-notify" "${DEST}/notify-discord"
+  ;;
+  * ) echo "Ok, no then :(";;
+esac

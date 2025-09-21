49b195bf0e79ec555289e8049d3c0ac9b2a87aef
Tomi Javanainen <tomi.javanainen@proton.me>
2025-09-21 21:03:41 +0300

    Fix install script

--- install.sh
+++ install.sh
@@ -6,13 +6,13 @@ if [[ -z "${VIRTUAL_ENV}" ]]; then
   exit 1
 fi
 
-read -pr "Install discord-notify (and link notify-discord) to ${DEST}? (y/n) " yn
+read -r -p "Install discord-notify (and link notify-discord) to ${DEST}? (y/n) " yn
 case $yn in
   [Yy]* )
     pyinstaller --onefile notify.py
-    cp -v dist/notify "${DEST}/discord-notify"
-    chmod -v $PERMS "${DEST}/discord-notify"
-    ln -s -f -v "${DEST}/discord-notify" "${DEST}/notify-discord"
+    sudo cp -v dist/notify "${DEST}/discord-notify"
+    sudo chmod -v $PERMS "${DEST}/discord-notify"
+    sudo ln -s -f -v "${DEST}/discord-notify" "${DEST}/notify-discord"
   ;;
   * ) echo "Ok, no then :(";;
 esac

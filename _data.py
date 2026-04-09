5ba8e54ae841c8c01b18bc80ef942be7ffa632a1
konso <tomi.javanainen@proton.me>
2026-04-09 21:25:10 +0300

    Fix truncation

--- _data.py
+++ _data.py
@@ -2,8 +2,9 @@ from _args import ParsedArgs
 
 
 def data(args: ParsedArgs) -> dict:
+    content = args.message
     # Discord rejects content with length over 2000
-    if len(args.message) > 2000:
+    if len(content) > 2000:
         print(f"WARNING: message length over 2000. Truncating.")
-        args.message = args.message[:1988] + " [truncated]"
-    return {"content": args.message}
+        content = content[:1988] + " [truncated]"
+    return {"content": content}

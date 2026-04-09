ae9a6fa829604342917b37785ea8b1cd378b9abe
konso <tomi.javanainen@proton.me>
2026-04-09 21:22:07 +0300

    Truncate messages to under 2000 length (Discord limit)

--- _data.py
+++ _data.py
@@ -2,5 +2,8 @@ from _args import ParsedArgs
 
 
 def data(args: ParsedArgs) -> dict:
-    return {'content': args.message}
-
+    # Discord rejects content with length over 2000
+    if len(args.message) > 2000:
+        print(f"WARNING: message length over 2000. Truncating.")
+        args.message = args.message[:1988] + " [truncated]"
+    return {"content": args.message}
